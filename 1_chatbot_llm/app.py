bfbb0e3ea2b35a818add9c9c4401ba73dd7b3b8a
Hazimleets <hazimansari88@gmail.com>
2025-08-19 11:35:54 +0500

    Pushing all files and folders for the hiring bot project

--- 1_chatbot_llm/app.py
+++ 1_chatbot_llm/app.py
@@ -4,8 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 app = Flask(__name__)
 app.register_blueprint(generate_bp)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "✅ Flask server is running! Send POST requests to /generate"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
